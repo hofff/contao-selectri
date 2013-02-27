7066bf34dbef17d9bb9338b27cf46ce5aa9a63cb
Oliver Hoff <oliver@hofff.com>
2013-02-27 19:31:51 +0100

    clear search and clear selection buttons

--- TL_ROOT/system/modules/backboneit_selectri/languages/en/default.php
+++ TL_ROOT/system/modules/backboneit_selectri/languages/en/default.php
@@ -5,7 +5,13 @@ $GLOBALS['TL_LANG']['stri']['select'] = 'Select';
 $GLOBALS['TL_LANG']['stri']['selectTitle'] = 'click to add to selection';
 $GLOBALS['TL_LANG']['stri']['deselect'] = 'Deselect';
 $GLOBALS['TL_LANG']['stri']['deselectTitle'] = 'click to remove from selection';
-$GLOBALS['TL_LANG']['stri']['empty'] = 'No selectable options available';
+
 $GLOBALS['TL_LANG']['stri']['search'] = 'Search...';
+$GLOBALS['TL_LANG']['stri']['clearSearchTitle'] = 'click to clear search';
+$GLOBALS['TL_LANG']['stri']['clearSearch'] = 'Clear search';
+$GLOBALS['TL_LANG']['stri']['clearSelectionTitle'] = 'click while holding down the shift key to clear all selected options';
+$GLOBALS['TL_LANG']['stri']['clearSelection'] = 'Clear selection';
 $GLOBALS['TL_LANG']['stri']['treeTitle'] = 'click to toggle';
 $GLOBALS['TL_LANG']['stri']['tree'] = 'Browse options';
+
+$GLOBALS['TL_LANG']['stri']['empty'] = 'No selectable options available';
