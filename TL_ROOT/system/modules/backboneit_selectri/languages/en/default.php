29777c4b8cd1a0b104a04e3a4910f6c907e3d4e8
Oliver Hoff <oliver@hofff.com>
2013-03-18 01:53:57 +0100

    optimize translation

--- TL_ROOT/system/modules/backboneit_selectri/languages/en/default.php
+++ TL_ROOT/system/modules/backboneit_selectri/languages/en/default.php
@@ -1,6 +1,6 @@
 <?php
 
-$GLOBALS['TL_LANG']['stri']['toggle'] = 'toggle node';
+$GLOBALS['TL_LANG']['stri']['toggleTitle'] = 'click to toggle the children of this node';
 $GLOBALS['TL_LANG']['stri']['pathTitle'] = 'click to show node in tree';
 $GLOBALS['TL_LANG']['stri']['select'] = 'Select';
 $GLOBALS['TL_LANG']['stri']['selectTitle'] = 'click to add to selection';
@@ -8,11 +8,12 @@ $GLOBALS['TL_LANG']['stri']['deselect'] = 'Deselect';
 $GLOBALS['TL_LANG']['stri']['deselectTitle'] = 'click to remove from selection';
 
 $GLOBALS['TL_LANG']['stri']['search'] = 'Search...';
-$GLOBALS['TL_LANG']['stri']['clearSearchTitle'] = 'click to clear search';
+$GLOBALS['TL_LANG']['stri']['searchEmpty'] = 'No results found for "%s"';
 $GLOBALS['TL_LANG']['stri']['clearSearch'] = 'Clear search';
-$GLOBALS['TL_LANG']['stri']['clearSelectionTitle'] = 'click while holding down the shift key to clear all selected options';
+$GLOBALS['TL_LANG']['stri']['clearSearchTitle'] = 'click to clear search';
 $GLOBALS['TL_LANG']['stri']['clearSelection'] = 'Clear selection';
-$GLOBALS['TL_LANG']['stri']['treeTitle'] = 'click to toggle';
+$GLOBALS['TL_LANG']['stri']['clearSelectionTitle'] = 'click while holding down the shift key to clear all selected options';
 $GLOBALS['TL_LANG']['stri']['tree'] = 'Browse options';
+$GLOBALS['TL_LANG']['stri']['treeTitle'] = 'click to toggle';
 
-$GLOBALS['TL_LANG']['stri']['empty'] = 'No selectable options available';
+$GLOBALS['TL_LANG']['stri']['noOptions'] = 'No selectable options available';
