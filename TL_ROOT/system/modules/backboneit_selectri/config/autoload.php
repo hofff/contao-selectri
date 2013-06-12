d65c664141f9a262e7aebf6bf39d7fd027bdc602
Oliver Hoff <oliver@hofff.com>
2013-06-12 20:12:36 +0200

    contao 3 compat (testing)

---
+++ TL_ROOT/system/modules/backboneit_selectri/config/autoload.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2013 Leo Feyer
+ *
+ * @package Backboneit_selectri
+ * @link    https://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	'SelectriAbstractDataFactory'    => 'system/modules/backboneit_selectri/SelectriAbstractDataFactory.php',
+	'SelectriContaoTableDataFactory' => 'system/modules/backboneit_selectri/SelectriContaoTableDataFactory.php',
+	'SelectriData'                   => 'system/modules/backboneit_selectri/SelectriData.php',
+	'SelectriDataFactory'            => 'system/modules/backboneit_selectri/SelectriDataFactory.php',
+	'SelectriLabelFormatter'         => 'system/modules/backboneit_selectri/SelectriLabelFormatter.php',
+	'SelectriNode'                   => 'system/modules/backboneit_selectri/SelectriNode.php',
+	'SelectriTableDataConfig'        => 'system/modules/backboneit_selectri/SelectriTableDataConfig.php',
+	'SelectriTableDataFactory'       => 'system/modules/backboneit_selectri/SelectriTableDataFactory.php',
+	'SelectriTableTreeData'          => 'system/modules/backboneit_selectri/SelectriTableTreeData.php',
+	'SelectriTableTreeDataNode'      => 'system/modules/backboneit_selectri/SelectriTableTreeDataNode.php',
+	'SelectriTreeIterator'           => 'system/modules/backboneit_selectri/SelectriTreeIterator.php',
+	'SelectriWidget'                 => 'system/modules/backboneit_selectri/SelectriWidget.php',
+));
+
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+	'selectri_children'  => 'system/modules/backboneit_selectri/templates',
+	'selectri_container' => 'system/modules/backboneit_selectri/templates',
+	'selectri_search'    => 'system/modules/backboneit_selectri/templates',
+));
